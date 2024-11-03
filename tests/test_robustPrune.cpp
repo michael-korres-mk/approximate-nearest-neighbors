125aa2fc0050a61c7f7aeed379137fa59f8209b0
mkorres <mkorres@iknowhow.com>
2024-11-03 20:54:06 +0200

    --- robustPrune and greedySearch tests

--- tests/test_robustPrune.cpp
+++ tests/test_robustPrune.cpp
@@ -1,43 +1,41 @@
 #include "../include/acutest.h"
 #include "../src/main/cpp/utility/Graph/Graph.h"
 #include <vector>
+#include <iostream>
+
+using namespace std;
 
 void test_robustPrune() {
-    Graph<int> graph({}, 1, 5, 3, 1.2);
-    vector<int> data0 = {0, 2};
-    vector<int> data1 = {2, 5};
-    vector<int> data2 = {2, 10};
-    vector<int> data3 = {-1, 8};
-    vector<int> data4 = {5, 6};
-    vector<int> data5 = {7, 20};
-    vector<int> data6 = {1, 2};
-    vector<int> data7 = {-7,2};
-    vector<int> data8 = {1, 8};
-    vector<int> data9 = {5, 2};
-    graph.addVertex(data0);        // ID = 0
-    graph.addVertex(data1);        // ID = 1
-    graph.addVertex(data2);        // ID = 2
-    graph.addVertex(data3);        // ID = 3
-    graph.addVertex(data4);        // ID = 4
-    graph.addVertex(data5);        // ID = 5
-    graph.addVertex(data6);        // ID = 6
-    graph.addVertex(data7);        // ID = 7
-    graph.addVertex(data8);        // ID = 8
-    graph.addVertex(data9);        // ID = 9
+    // Arrange
+    vector<int> data0 = {0, 0};
+    vector<int> data1 = {0, 1};
+    vector<int> data2 = {0, 2};
+    vector<int> data3 = {0, 3};
+    vector<int> data4 = {0, 4};
+    vector<int> data5 = {0, 5};
+    vector<int> data6 = {0, 6};
+    vector<int> data7 = {0, 7};
+    vector<int> data8 = {0, 8};
+    vector<int> data9 = {0, 9};
 
+    vector<vector<int>> data = {data0,data1,data2,data3,data4,data5,data6,data7,data8,data9};
 
-    graph.initializeRandomEdges();
+    Graph<int> graph(data, 1, 5, 3,3);
 
-    const auto& vertexMap = graph.getVertexMap();
-    for (const auto& [vertex, neighbors] : vertexMap) {
-        vector<Edge> outNeighbors = graph.getNeighbors(vertex);
-        for (const auto& neighbor : outNeighbors) {
-          cout << vertex << ", " << neighbor.getDestination() << endl;
-        }
+    int p = 0;
+    std::vector<int> V = graph.getVerticesIds();
+    double a = 3.0;
+    int R = 10;
+
+    std::vector<int> expectedNeighbors = {1, 2, 4, 7};
+
+    // Act
+    vector<Edge> result = graph.robustPrune(p,V,a,R);
+
+    for (int i = 0; i < result.size(); i++) {
+        TEST_ASSERT(result[i].getDestination() == expectedNeighbors[i]);
     }
 
-
-    TEST_ASSERT(false); // Εδώ θα προσθέσεις τον κανονικό κώδικα του test
 }
 
 TEST_LIST = {
